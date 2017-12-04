b5d70ce311b8dda678b120858afeccc7dbb31be0
jess <jess.monter@ciencias.unam.mx>
2017-12-03 22:24:17 -0600

    proyecto

--- t1/desclogit.py
+++ t1/desclogit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #-*-coding:utf8-*-
-
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from pylab import *
 
 def J(W,C,Y):
@@ -72,20 +73,32 @@ def grad_desc( W0,P, Y, ca, iters, verbose=True):
         if(verbose):
             if(i%100==0):
                 print("{0} : {1}".format(i,costo))
+
     return pesos[-1], costos
 
+def graph(costo, costos, inicio, fin):
+    
+    plt.plot(costo, costos, 'ro')
+    #plt.axis([inicio, fin, 0, 1])
+    plt.show()
+
 def ejemplo(iters=1000, ca=0.3):
-    P = genfromtxt('conjen_logit.csv', delimiter=',')
+    P = genfromtxt('conjuntoDatos.csv', delimiter=',')
+    #P = genfromtxt('conjen_logit.csv', delimiter=',')
     X = P[:,1:3]
     Xn = normalize(X)
     Xn = append( ones( (len(Xn),1) ), Xn, axis=1 )
+    #print X
     Y = P[:,-1]
+    #print Y
     W0 = ranf((3,))
     error_i = J(W0, Xn, Y)
     print "Descenso en w0={0}, w1={1}, w2={2},  error={3}".format(W0[0],W0[1],W0[2], error_i)
     Wf, costos = grad_desc( W0, Xn, Y, ca, iters )
     error_f = J(Wf,Xn, Y)
-    print u"Después de {0} iteraciones W_0={1}, W_1={2}, W_2={3}, error={4}".format(iters, Wf[0],Wf[1],Wf[2] ,error_f)
+    print "Después de {0} iteraciones W_0={1}, W_1={2}, W_2={3}, error={4}".format(iters, Wf[0],Wf[1],Wf[2] ,error_f)
+    graph(costos, costos, 2.1, 3.4)
+
 
 if __name__ =='__main__':
     iters = int(sys.argv[1])
